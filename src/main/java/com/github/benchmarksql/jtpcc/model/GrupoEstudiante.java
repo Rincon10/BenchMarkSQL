ba7942136d21483581fe3f7bf5e3dff27a46eb71
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-04-18 14:57:02 -0500

    Classes base code

---
+++ src/main/java/com/github/benchmarksql/jtpcc/model/GrupoEstudiante.java
@@ -0,0 +1,9 @@
+package com.github.benchmarksql.jtpcc.model;
+
+/**
+ * @author Iván Camilo Rincón Saavedra
+ * @author Leonardo Galeano
+ * @project benchmarksql
+ */
+public class GrupoEstudiante {
+}
