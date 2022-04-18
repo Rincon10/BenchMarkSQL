f3d22876515290ec828aac0b53b3c2e11b74f88d
Ersocaut <leonardo.soy.yo@hotmail.com>
2022-04-18 15:22:14 -0500

    Creation of our tables for benchmark

---
+++ sgbd/01-CreateTables.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS benchmarksql CASCADE;
+
+CREATE DATABASE benchmarksql;
+
+CREATE TABLE benchmarksql.materias(
+	m_sigla		varchar(10) NOT null,
+	m_nombre	varchar(25) NOT null,
+	m_creditos	integer NOT null
+) engine=innodb;
+
+CREATE TABLE benchmarksql.grupos(
+	g_m_id		varchar(10) NOT NULL,
+	g_id		integer NOT NULL,
+	g_numero	integer NOT NULL,
+) engine=innodb;
+
+CREATE TABLE benchamrksql.estudiantes(
+	e_id		integer NOT NULL,
+	e_nombre	varchar(25) NOT NULL,
+	e_apellidos	varchar(25) NOT NULL,
+	e_correo	varchar(50),
+	e_semestre	integer NOT NULL
+) engine=innodb;
+
+CREATE TABLE benchmarksql.salones(
+	s_id		integer NOT NULL,
+	s_edificio	varchar(1) NOT NULL,
+	s_numero	integer
+) engine=innodb;
+
+CREATE TABLE benchmarksql.salongrupo(
+	sg_s_id		integer NOT NULL,
+	sg_g_id		integer NOT NULL
+) engine=innodb;
+
+CREATE TABLE benchmarksql.grupoestudiante(
+	ge_e_id		integer NOT NULL,
+	ge_g_id		integer NOT NULL
+) engine=innodb;
