db82cd6c0513e7aaea97345ee18dfe566820ed93
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-04-18 15:56:08 -0500

    All classes

--- src/main/java/com/github/benchmarksql/jtpcc/model/GrupoEstudiante.java
+++ src/main/java/com/github/benchmarksql/jtpcc/model/GrupoEstudiante.java
@@ -1,9 +1,49 @@
 package com.github.benchmarksql.jtpcc.model;
 
+import java.io.Serializable;
+
 /**
  * @author Iván Camilo Rincón Saavedra
  * @author Leonardo Galeano
  * @project benchmarksql
  */
-public class GrupoEstudiante {
+public class GrupoEstudiante implements Serializable {
+    /**
+     * Generated Id.
+     */
+    private static final long serialVersionUID = -5735456083947093098L;
+    private int ge_e_id;
+    private int ge_g_id;
+
+    public GrupoEstudiante() {
+    }
+
+    public int getGe_e_id() {
+        return ge_e_id;
+    }
+
+    public void setGe_e_id(int ge_e_id) {
+        this.ge_e_id = ge_e_id;
+    }
+
+    public int getGe_g_id() {
+        return ge_g_id;
+    }
+
+    public void setGe_g_id(int ge_g_id) {
+        this.ge_g_id = ge_g_id;
+    }
+
+    /**
+     * describe al grupo de un estudiante.
+     */
+    @Override
+    public final String toString() {
+        final StringBuffer ret = new StringBuffer("");
+        ret.append("\n***************** GrupoEstudiante ********************");
+        ret.append("\n*           ge_e_id = " + this.getGe_e_id());
+        ret.append("\n*         ge_g_id = " + this.getGe_g_id());
+        ret.append("\n**********************************************");
+        return ret.toString();
+    }
 }
--- src/main/java/com/github/benchmarksql/jtpcc/model/SalonGrupo.java
+++ src/main/java/com/github/benchmarksql/jtpcc/model/SalonGrupo.java
@@ -1,9 +1,50 @@
 package com.github.benchmarksql.jtpcc.model;
 
+import java.io.Serializable;
+
 /**
  * @author Iván Camilo Rincón Saavedra
  * @author Leonardo Galeano
  * @project benchmarksql
  */
-public class SalonGrupo {
+public class SalonGrupo implements Serializable {
+
+    /**
+     * Generated Id.
+     */
+    private static final long serialVersionUID = -7555840082316032135L;
+    private int sg_s_id;
+    private int sg_g_id;
+
+    public SalonGrupo() {
+    }
+
+    public int getSg_s_id() {
+        return sg_s_id;
+    }
+
+    public void setSg_s_id(int sg_s_id) {
+        this.sg_s_id = sg_s_id;
+    }
+
+    public int getSg_g_id() {
+        return sg_g_id;
+    }
+
+    public void setSg_g_id(int sg_g_id) {
+        this.sg_g_id = sg_g_id;
+    }
+
+    /**
+     * describe al grupo de un estudiante.
+     */
+    @Override
+    public final String toString() {
+        final StringBuffer ret = new StringBuffer("");
+        ret.append("\n***************** SalonGrupo ********************");
+        ret.append("\n*           sg_s_id = " + this.getSg_s_id());
+        ret.append("\n*         sg_g_id = " + this.getSg_g_id());
+        ret.append("\n**********************************************");
+        return ret.toString();
+    }
 }
