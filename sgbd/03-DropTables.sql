8d55dae84b716e28ba323262aa131db194c7079a
Ersocaut <leonardo.soy.yo@hotmail.com>
2022-04-18 16:08:19 -0500

    Drops

---
+++ sgbd/03-DropTables.sql
@@ -0,0 +1,11 @@
+DROP TABLE benchmarksql.grupoestudiante;
+
+DROP TABLE benchmarksql.salongrupo;
+
+DROP TABLE benchmarksql.salones;
+
+DROP TABLE benchmarksql.estudiantes;
+
+DROP TABLE benchmarksql.grupos;
+
+DROP TABLE benchmarksql.materias;
