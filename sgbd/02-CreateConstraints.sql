398707a22ac3bd06f9beb152f3e18e5b215fe02a
Ersocaut <leonardo.soy.yo@hotmail.com>
2022-04-18 15:50:16 -0500

    Constraints

---
+++ sgbd/02-CreateConstraints.sql
@@ -0,0 +1,32 @@
+ALTER TABLE benchmarksql.materias ADD CONSTRAINT pk_materias
+	PRIMARY KEY (m_sigla);
+	
+ALTER TABLE benchmarksql.grupos ADD CONSTRAINT pk_grupos
+	PRIMARY KEY (g_id);
+	
+ALTER TABLE benchmarksql.estudiantes ADD CONSTRAINT pk_estudiantes
+	PRIMARY KEY (e_id);
+	
+ALTER TABLE benchmarksql.salones ADD CONSTRAINT pk_salones
+	PRIMARY KEY (s_id);
+	
+ALTER TABLE benchmarksql.salongrupo ADD CONSTRAINT pk_salongrupo
+	PRIMARY KEY (sg_s_id, sg_g_id);
+
+ALTER TABLE benchmarksql.grupoestudiante ADD CONSTRAINT pk_grupoestudiante
+	PRIMARY KEY (ge_e_id, ge_g_ig);
+	
+ALTER TABLE benchmarksql.grupos ADD CONSTRAINT fk_materia_grupo
+	FOREIGN KEY (g_m_id) REFERENCES benchmarksql.materias(m_sigla);
+	
+ALTER TABLE benchmarksql.salongrupo ADD CONSTRAINT fk_salongrupo_salon
+	FOREIGN KEY (sg_s_id) REFERENCES benchmarksql.salones(s_id);
+	
+ALTER TABLE benchmarksql.salongrupo ADD CONSTRAINT fk_salongrupo_grupo
+	FOREIGN KEY (sg_g_id) REFERENCES benchmarksql.grupos(g_id);
+	
+ALTER TABLE benchmarksql.grupoestudiante ADD CONSTRAINT fk_grupoestudiante_estudiante
+	FOREIGN KEY (ge_e_id) REFERENCES benchmarksql.estudiantes(e_id);
+	
+ALTER TABLE benchmarksql.grupoestudiante ADD CONSTRAINT fk_grupoestudiante_grupo
+	FOREIGN KEY (ge_g_id) REFERENCES benchmarksql.grupos(g_id);
